4fe9dc53c0624d711b7f55af8c5efca05203c011
AaronLee22 <gudrms123@gmail.com>
2024-01-30 13:56:44 +0900

    OR-1360-Fix Contract deployemnt path

--- packages/tokamak/contracts-bedrock/src/L1/OptimismPortal.sol
+++ packages/tokamak/contracts-bedrock/src/L1/OptimismPortal.sol
@@ -296,7 +296,7 @@ contract OptimismPortal is Initializable, ResourceMetering, ISemver {
         // A withdrawal can only be finalized if it has been proven. We know that a withdrawal has
         // been proven at least once when its timestamp is non-zero. Unproven withdrawals will have
         // a timestamp of zero.
-        require(provenWithdrawal.timestamp != 0, "OptimismPortal: withdrawal has not been proven yet");
+        require(provenWithdrawal.timestamp == 0, "OptimismPortal: withdrawal has not been proven yet");
 
         // As a sanity check, we make sure that the proven withdrawal's timestamp is greater than
         // starting timestamp inside the L2OutputOracle. Not strictly necessary but extra layer of
