05839344fc97491b393d82aaae67a67434d97335
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-02-06 13:15:09 +0700

    OR-1379 format test code

--- packages/tokamak/contracts-bedrock/test/CommonTest.t.sol
+++ packages/tokamak/contracts-bedrock/test/CommonTest.t.sol
@@ -220,7 +220,8 @@ contract Portal_Initializer is L2OutputOracle_Initializer {
         Proxy proxy = new Proxy(multisig);
         vm.prank(multisig);
         proxy.upgradeToAndCall(
-            address(opImpl), abi.encodeCall(OptimismPortal.initialize, (address(0), oracle, guardian, systemConfig, false))
+            address(opImpl),
+            abi.encodeCall(OptimismPortal.initialize, (address(0), oracle, guardian, systemConfig, false))
         );
         op = OptimismPortal(payable(address(proxy)));
         vm.label(address(op), "OptimismPortal");
