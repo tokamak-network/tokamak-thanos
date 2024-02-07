eb9df0717aa0d495191e0a8a3316e696ee53c896
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-02-07 14:50:01 +0700

    OR-1257 use common name for ton address

--- packages/tokamak/contracts-bedrock/test/OptimismPortal.t.sol
+++ packages/tokamak/contracts-bedrock/test/OptimismPortal.t.sol
@@ -920,7 +920,7 @@ contract OptimismPortalUpgradeable_Test is Portal_Initializer {
     function test_initialize_cannotInitProxy_reverts() external {
         vm.expectRevert("Initializable: contract is already initialized");
         OptimismPortal(payable(proxy)).initialize({
-            _tonAddress: address(0),
+            _nativeTokenAddress: address(0),
             _l2Oracle: L2OutputOracle(address(0)),
             _systemConfig: SystemConfig(address(0)),
             _guardian: address(0),
@@ -932,7 +932,7 @@ contract OptimismPortalUpgradeable_Test is Portal_Initializer {
     function test_initialize_cannotInitImpl_reverts() external {
         vm.expectRevert("Initializable: contract is already initialized");
         OptimismPortal(opImpl).initialize({
-            _tonAddress: address(0),
+            _nativeTokenAddress: address(0),
             _l2Oracle: L2OutputOracle(address(0)),
             _systemConfig: SystemConfig(address(0)),
             _guardian: address(0),
