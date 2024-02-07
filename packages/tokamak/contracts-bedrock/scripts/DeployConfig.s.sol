da0abb111f7c34077c4c45aaf77d15ed21207b11
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-02-07 14:50:01 +0700

    OR-1257 use common name for ton address

--- packages/tokamak/contracts-bedrock/scripts/DeployConfig.s.sol
+++ packages/tokamak/contracts-bedrock/scripts/DeployConfig.s.sol
@@ -16,7 +16,7 @@ contract DeployConfig is Script {
 
     address public finalSystemOwner;
     address public portalGuardian;
-    address public l1TONToken;
+    address public nativeTokenAddress;
     uint256 public l1ChainID;
     uint256 public l2ChainID;
     uint256 public l2BlockTime;
@@ -63,7 +63,7 @@ contract DeployConfig is Script {
 
         finalSystemOwner = stdJson.readAddress(_json, "$.finalSystemOwner");
         portalGuardian = stdJson.readAddress(_json, "$.portalGuardian");
-        l1TONToken = stdJson.readAddress(_json, "$.l1TONToken");
+        nativeTokenAddress = stdJson.readAddress(_json, "$.nativeTokenAddress");
         l1ChainID = stdJson.readUint(_json, "$.l1ChainID");
         l2ChainID = stdJson.readUint(_json, "$.l2ChainID");
         l2BlockTime = stdJson.readUint(_json, "$.l2BlockTime");
@@ -103,8 +103,8 @@ contract DeployConfig is Script {
         }
     }
 
-    function setL1TonAddress(address _l1TonToken) public {
-        l1TONToken = _l1TonToken;
+    function setNativeTokenAddress(address _nativeTokenAddress) public {
+        nativeTokenAddress = _nativeTokenAddress;
     }
 
     function l1StartingBlockTag() public returns (bytes32) {
