ea8a0ec9a82d44fed857ea305427fdedc8ed6adf
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-02-15 15:04:43 +0700

    OR-1387 add unpackOnApproveData

--- packages/tokamak/contracts-bedrock/src/L1/OnApprove.sol
+++ packages/tokamak/contracts-bedrock/src/L1/OnApprove.sol
@@ -8,11 +8,32 @@ abstract contract OnApprove is IERC165 {
         return interfaceId == (OnApprove(this).onApprove.selector);
     }
 
+    function pack(uint32 _minGasLimit, bytes memory _message) public pure returns(bytes memory) {
+        bytes memory packedData = abi.encode(_minGasLimit, _message);
+        return packedData;
+    }
+
+    function unpackOnApproveData(bytes memory _data) public pure returns(uint32, bytes memory) {
+        require(_data.length >= 4, "");
+        uint32 _minGasLimit;
+        bytes memory _message =_data;
+        uint256 _messageLength;
+
+        assembly {
+            let _pos := _data
+            _messageLength := sub(mload(_pos), 32)
+            _pos := add(_pos, 32)
+            _minGasLimit := shr(224, mload(_pos))
+            _message := add(_pos, 4)
+        }
+        return (_minGasLimit, _message);
+    }
+
     function onApprove(
         address _owner,
         address _spender,
         uint256 _amount,
-        bytes calldata data
+        bytes memory _data
     )
         external
         virtual
