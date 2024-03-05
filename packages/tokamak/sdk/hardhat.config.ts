984bf0f9141b96990dc1aa1eb5d016c29374b318
Theo Lee <theo.l@onther.io>
2024-03-05 16:58:37 +0900

    use dotenv

--- packages/tokamak/sdk/hardhat.config.ts
+++ packages/tokamak/sdk/hardhat.config.ts
@@ -4,7 +4,7 @@ import { ethers } from 'ethers'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-deploy'
-
+import 'dotenv/config'
 import './tasks'
 
 const config: HardhatUserConfig = {
