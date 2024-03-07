318cfbcf200aeb21886bacb4269af473c9a6ede2
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-03-07 11:26:52 +0700

    OR-1419 update onApprove calldata layout

--- packages/tokamak/contracts-bedrock/src/L1/L1CrossDomainMessenger.sol
+++ packages/tokamak/contracts-bedrock/src/L1/L1CrossDomainMessenger.sol
@@ -74,20 +74,22 @@ contract L1CrossDomainMessenger is CrossDomainMessenger, OnApprove, ISemver {
 
     /// @notice unpack onApprove data
     /// @param _data     Data used in OnApprove contract
-    function unpackOnApproveData(bytes calldata _data) public pure returns (address _to, uint32 _minGasLimit, bytes calldata _message) {
-        require(_data.length >= 24, "On approve data for cdm is too short");
+    function unpackOnApproveData(bytes calldata _data) public pure returns (address _from, address _to, uint256 _amount, uint32 _minGasLimit, bytes calldata _message) {
+        require(_data.length >= 76, "On approve data for L1StandardBridge is too short");
         assembly {
-            // The layout of a "bytes calldata" is:
-            // The next 20 bytes: _to
-            // The next 4 bytes: _minGasLimit
-            // The rest: _message
-
-            // Get _data.length
-            _to := shr(96, calldataload(_data.offset))
-            _minGasLimit := shr(224, shl(160, calldataload(_data.offset)))
-            _message.offset := add(_data.offset, 24)
-            _message.length := sub(_data.length, 24)
-        }
+                // The layout of a "bytes calldata" is:
+                // The first 20 bytes: _from
+                // The next 20 bytes: _to
+                // The next 32 bytes: _amount
+                // The next 4 bytes: _minGasLimit
+                // The rest: _message
+                _from := shr(96, calldataload(_data.offset))
+                _to := shr(96, calldataload(add(_data.offset, 20)))
+                _amount := shr(96, calldataload(add(_data.offset, 40)))
+                _minGasLimit := shr(224, calldataload(add(_data.offset, 72)))
+                _message.offset := add(_data.offset, 76)
+                _message.length := sub(_data.length, 76)
+            }
     }
 
     /// @notice ERC20 onApprove callback
@@ -105,8 +107,10 @@ contract L1CrossDomainMessenger is CrossDomainMessenger, OnApprove, ISemver {
         returns (bool)
     {
         require(msg.sender == address(nativeTokenAddress), "only accept native token approve callback");
-        (address _to, uint32 _minGasLimit, bytes calldata _message) = unpackOnApproveData(_data);
-        _sendNativeTokenMessage(_owner, _to, _amount, _minGasLimit, _message);
+        (address from, address to, uint256 amount, uint32 minGasLimit, bytes calldata message) = unpackOnApproveData(_data);
+        require(_owner == from, "invalid encoded data: from");
+        require(_amount == amount, "invalid encoded data: amount");
+        _sendNativeTokenMessage(from, to, amount, minGasLimit, message);
         return true;
     }
 
