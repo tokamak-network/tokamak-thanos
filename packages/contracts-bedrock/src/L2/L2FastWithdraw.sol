cd14a19466782eb594e80d0b564ae0d0ec5e8342
HyukSang <zzooppii@gmail.com>
2024-03-13 16:25:50 +0900

    develop the L2FastWithdraw

---
+++ packages/contracts-bedrock/src/L2/L2FastWithdraw.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.15;
+
+import "./L2FastWithdrawStorage";
+
+interface IL2StandardBridge {
+    function withdrawTo(
+        address _l2Token,
+        address _to,
+        uint256 _amount,
+        uint32 _minGasLimit,
+        bytes calldata _extraData
+    )
+        external
+        payable;
+}
+
+
+contract L2FastWithdraw is L2FastWithdrawStorage {
+    //use Owner
+    function setL2StandardBridge(
+        address _l2StandardBridge
+    ) external {
+        L2StandradBridge = _l2StandardBridge;
+    }
+
+    function setL1FwContract(
+        address _l1fwAddr
+    ) external {
+        L1fwContract = _l1fwAddr;
+    }
+
+
+    //use User
+    function applicationFW(
+        address _l2Token,
+        uint256 _amount,
+        uint256 _minAmount
+    ) external {
+        //make the extraData (msg.sender, amount)
+        //check minGasLimit
+        IL2StandardBridge(L2StandradBridge).withdrawTo(_l2Token, L1fwContract, _amount, _minGasLimit, _extraData);
+
+        //sendMessage to L1FwContract;
+    }
+}
---
+++ packages/contracts-bedrock/src/L2/L2FastWithdrawStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.15;
+
+contract L2FastWithdrawStorage {
+
+    //L1 information
+    address public L1fwContract;
+
+
+    //L2 information
+    address public L2StandradBridge;
+
+}
