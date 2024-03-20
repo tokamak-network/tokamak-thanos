227102e7d00ae4f4636caf37efe40dbe8036fb13
0x6e616d <nam@tokamak.network>
2024-03-20 08:09:46 +0700

    fix: make devet-up with fork public network

--- ops-bedrock/entrypoint-fork-l1.sh
+++ ops-bedrock/entrypoint-fork-l1.sh
@@ -2,15 +2,22 @@
 set -exu
 
 GENESIS_FILE_PATH="${GENESIS_FILE_PATH:-/genesis.json}"
-CHAIN_ID=$(jq -r .config.chainId "$GENESIS_FILE_PATH" || echo "1")  # This line reads chainId from genesis file or defaults to "1" if the file is not present
+CHAIN_ID=$(jq -r .config.chainId "$GENESIS_FILE_PATH")
+GAS_LIMIT=$(jq -r .gasLimit "$GENESIS_FILE_PATH")
+GAS_LIMIT_VALUE=$(echo "GAS_LIMIT" | awk '{ printf("%d", $0) }')
+
 RPC_PORT="${RPC_PORT:-8545}"
 WS_PORT="${WS_PORT:-8546}"
-L1_RPC="${L1_RPC:-https://eth.llamarpc.com}"
+L1_RPC="${L1_RPC:-https://eth-pokt.nodies.app}"
+BLOCK_NUMBER=${BLOCK_NUMBER:-19472106}
 
 exec anvil \
   --fork-url "$L1_RPC" \
+  --fork-block-number "$BLOCK_NUMBER" \
   --host "0.0.0.0" \
   --port "$RPC_PORT" \
+  --base-fee "1" \
+  --gas-limit "$GAS_LIMIT_VALUE" \
   --chain-id "$CHAIN_ID" \
   "$@"
 
