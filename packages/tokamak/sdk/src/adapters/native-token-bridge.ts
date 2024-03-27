1c68997deadd233b07f0e5a75664e2254883ca03
rlgns98kr <rlgns98kr@gmail.com>
2024-03-27 06:25:20 +0000

    merge pr 110

--- packages/tokamak/sdk/src/adapters/native-token-bridge.ts
+++ packages/tokamak/sdk/src/adapters/native-token-bridge.ts
@@ -14,9 +14,9 @@ import {
 } from '@ethersproject/abstract-provider'
 import { predeploys } from '@eth-optimism/contracts'
 import { hexStringEquals } from '@tokamak-network/core-utils'
-import l1StandardBridgeArtifact from '@eth-optimism/contracts-bedrock/forge-artifacts/L1StandardBridge.sol/L1StandardBridge.json'
-import l2StandardBridgeArtifact from '@eth-optimism/contracts-bedrock/forge-artifacts/L2StandardBridge.sol/L2StandardBridge.json'
-import optimismMintableERC20 from '@eth-optimism/contracts-bedrock/forge-artifacts/OptimismMintableERC20.sol/OptimismMintableERC20.json'
+import l1StandardBridgeArtifact from '@tokamak-network/thanos-contracts/forge-artifacts/L1StandardBridge.sol/L1StandardBridge.json'
+import l2StandardBridgeArtifact from '@tokamak-network/thanos-contracts/forge-artifacts/L2StandardBridge.sol/L2StandardBridge.json'
+import optimismMintableERC20 from '@tokamak-network/thanos-contracts/forge-artifacts/OptimismMintableERC20.sol/OptimismMintableERC20.json'
 
 import { CrossChainMessenger } from '../cross-chain-messenger'
 import {
