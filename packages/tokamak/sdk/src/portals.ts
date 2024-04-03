e6622050f9579901612d967a18c72ad6c4cb4a13
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-04-03 18:31:23 +0700

    OR-1446 add prove for sdk's portal

--- packages/tokamak/sdk/src/portals.ts
+++ packages/tokamak/sdk/src/portals.ts
@@ -6,8 +6,8 @@ import {
   TransactionReceipt,
 } from '@ethersproject/abstract-provider'
 import { Signer } from '@ethersproject/abstract-signer'
-import { BigNumber, ethers } from 'ethers'
-import { sleep, DepositTx } from '@tokamak-network/core-utils'
+import { BigNumber, PayableOverrides, ethers } from 'ethers'
+import { sleep, DepositTx, toRpcHexString } from '@tokamak-network/core-utils'
 
 import {
   DepositTransactionRequest,
@@ -16,8 +16,14 @@ import {
   NumberLike,
   SignerOrProviderLike,
   ProvenWithdrawal,
+  WithdrawalMessageInfo,
+  WithdrawalTransactionRequest,
 } from './interfaces'
 import {
+  calculateWithdrawalMessage,
+  hashLowLevelMessage,
+  hashMessageHash,
+  makeStateTrieProof,
   toSignerOrProvider,
   toNumber,
   DeepPartial,
@@ -202,6 +208,54 @@ export class Portals {
     return promiseString
   }
 
+  public async getL2BlockNumber(): Promise<number> {
+    return this.contracts.l1.L2OutputOracle.latestBlockNumber()
+  }
+
+  public async calculateWithdrawalMessage(
+    txReceipt: TransactionReceipt
+  ): Promise<WithdrawalMessageInfo> {
+    console.log('[calculateWithdrawalMessage]')
+    if (txReceipt.status !== 1) {
+      return null
+    }
+    let promiseMessage: Promise<WithdrawalMessageInfo> = null
+    txReceipt.logs.forEach((log) => {
+      console.log('log:', log)
+      if (
+        log.topics[0] ===
+        ethers.utils.id(
+          'MessagePassed(uint256,address,address,uint256,uint256,bytes,bytes32)'
+        )
+      ) {
+        const withdrawalMessage = calculateWithdrawalMessage(log)
+        promiseMessage = Promise.resolve(withdrawalMessage)
+        console.log('Found withdrawal event:', withdrawalMessage)
+      }
+    })
+    return promiseMessage
+  }
+
+  public async waitForWithdrawalTxReadyForRelay(
+    txReceipt: TransactionReceipt,
+    opts?: {
+      pollIntervalMs?: number
+      timeoutMs?: number
+    }
+  ) {
+    const l2BlockNumber = txReceipt.blockNumber
+    let totalTimeMs = 0
+    while (totalTimeMs < (opts?.timeoutMs || Infinity)) {
+      const tick = Date.now()
+      if (l2BlockNumber <= (await this.getL2BlockNumber())) {
+        return
+      }
+      await sleep(opts?.pollIntervalMs || 1000)
+      totalTimeMs += Date.now() - tick
+    }
+    throw new Error(`timed out waiting for relayed deposit transaction`)
+  }
+
   /**
    * Queries the current challenge period in seconds from the StateCommitmentChain.
    *
@@ -248,6 +302,40 @@ export class Portals {
     return (opts?.signer || this.l1Signer).sendTransaction(tx)
   }
 
+  public async initiateWithdrawal(
+    request: WithdrawalTransactionRequest,
+    opts?: {
+      signer?: Signer
+    }
+  ): Promise<TransactionResponse> {
+    const tx = await this.populateTransaction.initiateWithdrawal(request)
+    return (opts?.signer || this.l2Signer).sendTransaction(tx)
+  }
+
+  public async proveWithdrawalTransaction(
+    message: WithdrawalMessageInfo,
+    opts?: {
+      signer?: Signer
+    }
+  ): Promise<TransactionResponse> {
+    const tx = await this.populateTransaction.proveWithdrawalTransaction(
+      message
+    )
+    return (opts?.signer || this.l1Signer).sendTransaction(tx)
+  }
+
+  public async finalizeWithdrawalTransaction(
+    message: WithdrawalMessageInfo,
+    opts?: {
+      signer?: Signer
+    }
+  ): Promise<TransactionResponse> {
+    const tx = await this.populateTransaction.finalizeWithdrawalTransaction(
+      message
+    )
+    return (opts?.signer || this.l2Signer).sendTransaction(tx)
+  }
+
   populateTransaction = {
     depositTransaction: async (
       request: DepositTransactionRequest
@@ -258,6 +346,88 @@ export class Portals {
         request.gasLimit,
         request.data
       )
+    },
+
+    initiateWithdrawal: async (
+      request: WithdrawalTransactionRequest
+    ): Promise<TransactionRequest> => {
+      return this.contracts.l2.BedrockMessagePasser.populateTransaction.initiateWithdrawal(
+        request.target,
+        request.gasLimit,
+        request.data,
+        {
+          value: request.value,
+        }
+      )
+    },
+
+    proveWithdrawalTransaction: async (
+      message: WithdrawalMessageInfo,
+      opts?: {
+        overrides?: PayableOverrides
+      }
+    ): Promise<TransactionRequest> => {
+      const l2OutputIndex =
+        await this.contracts.l1.L2OutputOracle.getL2OutputIndexAfter(
+          message.l2BlockNumber
+        )
+      const proposal = await this.contracts.l1.L2OutputOracle.getL2Output(
+        l2OutputIndex
+      )
+
+      const withdrawalInfoSlot = hashMessageHash(
+        hashLowLevelMessage({
+          message: message.message,
+          messageNonce: message.messageNonce,
+          minGasLimit: message.minGasLimit,
+          value: message.value,
+          sender: message.sender,
+          target: message.target,
+        })
+      )
+
+      const storageProof = await makeStateTrieProof(
+        this.l2Provider as ethers.providers.JsonRpcProvider,
+        proposal.l2BlockNumber,
+        this.contracts.l2.BedrockMessagePasser.address,
+        withdrawalInfoSlot
+      )
+
+      const block = await (
+        this.l2Provider as ethers.providers.JsonRpcProvider
+      ).send('eth_getBlockByNumber', [
+        toRpcHexString(proposal.l2BlockNumber),
+        false,
+      ])
+
+      const args = [
+        [
+          message.messageNonce,
+          message.sender,
+          message.target,
+          message.value,
+          message.minGasLimit,
+          message.message,
+        ],
+        l2OutputIndex.toNumber(),
+        [
+          ethers.constants.HashZero,
+          block.stateRoot,
+          storageProof.storageRoot, // for proving storage root is correct
+          block.hash,
+        ],
+        storageProof.storageProof, // for proving withdrawal info
+        opts?.overrides || {},
+      ] as const
+      return this.contracts.l1.OptimismPortal.populateTransaction.proveWithdrawalTransaction(
+        ...args
+      )
+    },
+
+    finalizeWithdrawalTransaction: async (
+      message: WithdrawalMessageInfo
+    ): Promise<TransactionRequest> => {
+      return null
     },
   }
 }
