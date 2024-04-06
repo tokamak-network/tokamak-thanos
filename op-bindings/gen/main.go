c2cafdc289b644c6723a9d1d11ae18a3927389c2
AaronLee22 <gudrms123@gmail.com>
2024-04-06 19:33:24 +0900

    Update completed version of bindings

--- op-bindings/gen/main.go
+++ op-bindings/gen/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
-	"html/template"
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
+	"regexp"
 	"strings"
+	"text/template"
 
+	"github.com/ethereum-optimism/optimism/op-bindings/ast"
 	"github.com/ethereum-optimism/optimism/op-bindings/foundry"
 	"github.com/ethereum-optimism/optimism/op-bindings/hardhat"
 )
@@ -60,61 +64,158 @@ func main() {
 
 // processForgeArtifacts processes Forge artifacts to generate Go bindings.
 func processForgeArtifacts(f flags) {
-	// Read the contracts file.
+	if f.MonorepoBase == "" {
+		log.Fatal("must provide -monorepo-base")
+	}
+	log.Printf("Using monorepo base %s\n", f.MonorepoBase)
+
 	contractData, err := os.ReadFile(f.ForgeContracts)
 	if err != nil {
-		log.Fatalf("error reading contracts file: %v", err)
+		log.Fatal("error reading contract list: %w\n", err)
 	}
-	var contracts []string
-	// Unmarshal the JSON data into the contracts slice.
+	contracts := []string{}
 	if err := json.Unmarshal(contractData, &contracts); err != nil {
-		log.Fatalf("error parsing contracts JSON: %v", err)
+		log.Fatal("error parsing contract list: %w\n", err)
 	}
 
-	// Create a set of source maps.
 	sourceMaps := strings.Split(f.SourceMaps, ",")
 	sourceMapsSet := make(map[string]struct{})
-	for _, contractName := range sourceMaps {
-		sourceMapsSet[contractName] = struct{}{}
+	for _, k := range sourceMaps {
+		sourceMapsSet[k] = struct{}{}
 	}
 
-	// Generate Go bindings for each contract.
-	for _, contract := range contracts {
-		log.Printf("generating code for : %s", contract)
-		artifactPath := filepath.Join(f.ForgeArtifacts, contract+".sol", contract+".json")
-		forgeArtifactData, err := os.ReadFile(artifactPath)
-		if err != nil {
-			log.Printf("cannot find forge-artifact for %s, %v", contract, err)
-			continue
-		}
-		log.Printf("using forge-artifact %s\n", artifactPath)
+	if len(contracts) == 0 {
+		log.Fatalf("must define a list of contracts")
+	}
 
+	t := template.Must(template.New("artifact").Parse(tmpl))
+
+	// Make a temp dir to hold all the inputs for abigen
+	dir, err := os.MkdirTemp("", "op-bindings")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Using package %s\n", f.Package)
+
+	defer os.RemoveAll(dir)
+	log.Printf("created temp dir %s\n", dir)
+
+	// If some contracts have the same name then the path to their
+	// artifact depends on their full import path. Scan over all artifacts
+	// and hold a mapping from the contract name to the contract path.
+	// Walk walks the directory deterministically, so the later instance
+	// of the contract with the same name will be used
+	re := regexp.MustCompile(`\.\d+\.\d+\.\d+`)
+	artifactPaths := make(map[string]string)
+	if err := filepath.Walk(f.ForgeArtifacts,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if strings.HasSuffix(path, ".json") {
+				base := filepath.Base(path)
+				name := strings.TrimSuffix(base, ".json")
+
+				// remove the compiler version from the name
+				sanitized := re.ReplaceAllString(name, "")
+				if _, ok := artifactPaths[sanitized]; !ok {
+					artifactPaths[sanitized] = path
+				}
+			}
+			return nil
+		}); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, name := range contracts {
+		log.Printf("generating code for %s\n", name)
+
+		artifactPath := path.Join(f.ForgeArtifacts, name+".sol", name+".json")
+		forgeArtifactData, err := os.ReadFile(artifactPath)
+		if errors.Is(err, os.ErrNotExist) {
+			log.Printf("cannot find forge-artifact for %s at standard path %s, trying %s\n", name, artifactPath, artifactPaths[name])
+			artifactPath = artifactPaths[name]
+			forgeArtifactData, err = os.ReadFile(artifactPath)
+			if errors.Is(err, os.ErrNotExist) {
+				log.Fatalf("cannot find forge-artifact of %q\n", name)
+			}
+		}
+
+		log.Printf("using forge-artifact %s\n", artifactPath)
 		var artifact foundry.Artifact
 		if err := json.Unmarshal(forgeArtifactData, &artifact); err != nil {
-			log.Fatalf("Failed to unmarshal artifact for contract %s: %v", contract, err)
+			log.Fatalf("failed to parse forge artifact of %q: %v\n", name, err)
 		}
 
-		// Write ABI and BIN files.
-		abiPath := filepath.Join(f.OutDir, contract+".abi")
-		if err := os.WriteFile(abiPath, artifact.Abi, 0644); err != nil {
-			log.Fatalf("Failed to write ABI file for %s: %v", contract, err)
+		rawAbi := artifact.Abi
+		if err != nil {
+			log.Fatalf("error marshaling abi: %v\n", err)
+		}
+		abiFile := path.Join(dir, name+".abi")
+		if err := os.WriteFile(abiFile, rawAbi, 0o600); err != nil {
+			log.Fatalf("error writing file: %v\n", err)
+		}
+		rawBytecode := artifact.Bytecode.Object.String()
+		if err != nil {
+			log.Fatalf("error marshaling bytecode: %v\n", err)
+		}
+		bytecodeFile := path.Join(dir, name+".bin")
+		if err := os.WriteFile(bytecodeFile, []byte(rawBytecode), 0o600); err != nil {
+			log.Fatalf("error writing file: %v\n", err)
 		}
 
-		binPath := filepath.Join(f.OutDir, contract+".bin")
-		if err := os.WriteFile(binPath, []byte(artifact.Bytecode.Object.String()), 0644); err != nil {
-			log.Fatalf("Failed to write BIN file for %s: %v", contract, err)
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Fatalf("error getting cwd: %v\n", err)
 		}
 
-		// Use abigen to generate Go bindings.
-		outFile := filepath.Join(f.OutDir, contract+".go")
-		cmd := exec.Command("abigen", "--abi", abiPath, "--bin", binPath, "--pkg", f.Package, "--type", contract, "--out", outFile)
+		lowerName := strings.ToLower(name)
+		outFile := path.Join(cwd, f.Package, lowerName+".go")
+
+		cmd := exec.Command("abigen", "--abi", abiFile, "--bin", bytecodeFile, "--pkg", f.Package, "--type", name, "--out", outFile)
 		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
 
 		if err := cmd.Run(); err != nil {
-			log.Fatalf("abigen failed for contract %s: %v", contract, err)
+			log.Fatalf("error running abigen: %v\n", err)
 		}
-		log.Printf("wrote file bindings: %s", contract)
+
+		storage := artifact.StorageLayout
+		canonicalStorage := ast.CanonicalizeASTIDs(&storage, f.MonorepoBase)
+		ser, err := json.Marshal(canonicalStorage)
+		if err != nil {
+			log.Fatalf("error marshaling storage: %v\n", err)
+		}
+		serStr := strings.Replace(string(ser), "\"", "\\\"", -1)
+
+		deployedSourceMap := ""
+		if _, ok := sourceMapsSet[name]; ok {
+			deployedSourceMap = artifact.DeployedBytecode.SourceMap
+		}
+
+		d := data{
+			Name:              name,
+			StorageLayout:     serStr,
+			DeployedBin:       artifact.DeployedBytecode.Object.String(),
+			Package:           f.Package,
+			DeployedSourceMap: deployedSourceMap,
+		}
+
+		fname := filepath.Join(f.OutDir, strings.ToLower(name)+"_more.go")
+		outfile, err := os.OpenFile(
+			fname,
+			os.O_RDWR|os.O_CREATE|os.O_TRUNC,
+			0o600,
+		)
+		if err != nil {
+			log.Fatalf("error opening %s: %v\n", fname, err)
+		}
+
+		if err := t.Execute(outfile, d); err != nil {
+			log.Fatalf("error writing template %s: %v", outfile.Name(), err)
+		}
+		outfile.Close()
+		log.Printf("wrote file %s\n", outfile.Name())
 	}
 }
 
@@ -212,9 +313,7 @@ const {{.Name}}StorageLayoutJSON = "{{.StorageLayout}}"
 var {{.Name}}StorageLayout = new(solc.StorageLayout)
 
 var {{.Name}}DeployedBin = "{{.DeployedBin}}"
-{{if .DeployedSourceMap}}
-var {{.Name}}DeployedSourceMap = "{{.DeployedSourceMap}}"
-{{end}}
+
 func init() {
 	if err := json.Unmarshal([]byte({{.Name}}StorageLayoutJSON), {{.Name}}StorageLayout); err != nil {
 		panic(err)
