71c242e96a26eab3456b3bdc3b4fa9fdd1ead73c
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-05-10 11:29:17 +0700

    OR-1587 update common tests

--- packages/tokamak/contracts-bedrock/test/CrossDomainOwnable.t.sol
+++ packages/tokamak/contracts-bedrock/test/CrossDomainOwnable.t.sol
@@ -68,6 +68,7 @@ contract CrossDomainOwnableThroughPortal_Test is Portal_Initializer {
             _mint: 0,
             _value: 0,
             _gasLimit: 30_000,
+            _isCreation: false,
             _data: abi.encodeWithSelector(XDomainSetter.set.selector, 1)
         });
 
