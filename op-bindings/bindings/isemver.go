4bca1886f4d3c443eeb10fad19c14d8ba141756d
Theo Lee <theo.l@onther.io>
2024-05-16 15:57:36 +0900

    update bindings

--- op-bindings/bindings/isemver.go
+++ op-bindings/bindings/isemver.go
@@ -26,12 +26,11 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
-	_ = abi.ConvertType
 )
 
 // ISemverMetaData contains all meta data concerning the ISemver contract.
 var ISemverMetaData = &bind.MetaData{
-	ABI: "[{\"inputs\":[],\"name\":\"version\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
+	ABI: "[{\"type\":\"function\",\"name\":\"version\",\"inputs\":[],\"outputs\":[{\"name\":\"\",\"type\":\"string\",\"internalType\":\"string\"}],\"stateMutability\":\"view\"}]",
 }
 
 // ISemverABI is the input ABI used to generate the binding from.
@@ -135,11 +134,11 @@ func NewISemverFilterer(address common.Address, filterer bind.ContractFilterer) 
 
 // bindISemver binds a generic wrapper to an already deployed contract.
 func bindISemver(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := ISemverMetaData.GetAbi()
+	parsed, err := abi.JSON(strings.NewReader(ISemverABI))
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
