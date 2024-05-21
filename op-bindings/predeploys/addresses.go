0d6481038cef6e80612f7c694c8ec1c19d333a29
Theo Lee <theo.l@onther.io>
2024-05-21 19:44:30 +0900

    add predeploys

--- op-bindings/predeploys/addresses.go
+++ op-bindings/predeploys/addresses.go
@@ -26,7 +26,6 @@ const (
 	SchemaRegistry                     = "0x4200000000000000000000000000000000000020"
 	EAS                                = "0x4200000000000000000000000000000000000021"
 	ETH                                = "0x4200000000000000000000000000000000000486"
-	Permit2                            = "0x4200000000000000000000000000000000000501"
 	QuoterV2                           = "0x4200000000000000000000000000000000000502"
 	SwapRouter02                       = "0x4200000000000000000000000000000000000503"
 	UniswapV3Factory                   = "0x4200000000000000000000000000000000000504"
@@ -39,6 +38,17 @@ const (
 	SignatureChecker                   = "0x4200000000000000000000000000000000000776"
 	MasterMinter                       = "0x4200000000000000000000000000000000000777"
 	FiatTokenV2_2                      = "0x4200000000000000000000000000000000000778"
+	Create2Deployer                    = "0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2"
+	MultiCall3                         = "0xcA11bde05977b3631167028862bE2a173976CA11"
+	Safe_v130                          = "0x69f4D1788e39c87893C980c06EdF4b7f686e2938"
+	SafeL2_v130                        = "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA"
+	MultiSendCallOnly_v130             = "0xA1dabEF33b3B82c7814B6D82A79e50F4AC44102B"
+	SafeSingletonFactory               = "0x914d7Fec6aaC8cd542e72Bca78B30650d45643d7"
+	DeterministicDeploymentProxy       = "0x4e59b44847b379578588920cA78FbF26c0B4956C"
+	MultiSend_v130                     = "0x998739BFdAAdde7C933B942a68053933098f9EDa"
+	Permit2                            = "0x000000000022D473030F116dDEE9F6B43aC78BA3"
+	SenderCreator                      = "0x7fc98430eaedbb6070b35b39d798725049088348"
+	EntryPoint                         = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
 )
 
 var (
@@ -62,7 +72,6 @@ var (
 	SchemaRegistryAddr                     = common.HexToAddress(SchemaRegistry)
 	EASAddr                                = common.HexToAddress(EAS)
 	ETHAddr                                = common.HexToAddress(ETH)
-	Permit2Addr                            = common.HexToAddress(Permit2)
 	QuoterV2Addr                           = common.HexToAddress(QuoterV2)
 	SwapRouter02Addr                       = common.HexToAddress(SwapRouter02)
 	UniswapV3FactoryAddr                   = common.HexToAddress(UniswapV3Factory)
@@ -75,58 +84,123 @@ var (
 	SignatureCheckerAddr                   = common.HexToAddress(SignatureChecker)
 	MasterMinterAddr                       = common.HexToAddress(MasterMinter)
 	FiatTokenV2_2Addr                      = common.HexToAddress(FiatTokenV2_2)
+	Create2DeployerAddr                    = common.HexToAddress(Create2Deployer)
+	MultiCall3Addr                         = common.HexToAddress(MultiCall3)
+	Safe_v130Addr                          = common.HexToAddress(Safe_v130)
+	SafeL2_v130Addr                        = common.HexToAddress(SafeL2_v130)
+	MultiSendCallOnly_v130Addr             = common.HexToAddress(MultiSendCallOnly_v130)
+	SafeSingletonFactoryAddr               = common.HexToAddress(SafeSingletonFactory)
+	DeterministicDeploymentProxyAddr       = common.HexToAddress(DeterministicDeploymentProxy)
+	MultiSend_v130Addr                     = common.HexToAddress(MultiSend_v130)
+	Permit2Addr                            = common.HexToAddress(Permit2)
+	SenderCreatorAddr                      = common.HexToAddress(SenderCreator)
+	EntryPointAddr                         = common.HexToAddress(EntryPoint)
 
-	Predeploys = make(map[string]*common.Address)
+	Predeploys          = make(map[string]*Predeploy)
+	PredeploysByAddress = make(map[common.Address]*Predeploy)
 )
 
-// IsProxied returns true for predeploys that will sit behind a proxy contract
-func IsProxied(predeployAddr common.Address) bool {
-	switch predeployAddr {
-	case LegacyERC20NativeTokenAddr:
-	case WNativeTokenAddr:
-	case GovernanceTokenAddr:
-	case ETHAddr:
-	case SignatureCheckerAddr:
-	case MasterMinterAddr:
-	default:
-		return true
-	}
-	return false
-}
-
 func init() {
-	Predeploys["LegacyERC20NativeToken"] = &LegacyERC20NativeTokenAddr
-	Predeploys["L2ToL1MessagePasser"] = &L2ToL1MessagePasserAddr
-	Predeploys["DeployerWhitelist"] = &DeployerWhitelistAddr
-	Predeploys["WNativeToken"] = &WNativeTokenAddr
-	Predeploys["L2CrossDomainMessenger"] = &L2CrossDomainMessengerAddr
-	Predeploys["L2StandardBridge"] = &L2StandardBridgeAddr
-	Predeploys["SequencerFeeVault"] = &SequencerFeeVaultAddr
-	Predeploys["OptimismMintableERC20Factory"] = &OptimismMintableERC20FactoryAddr
-	Predeploys["L1BlockNumber"] = &L1BlockNumberAddr
-	Predeploys["GasPriceOracle"] = &GasPriceOracleAddr
-	Predeploys["L1Block"] = &L1BlockAddr
-	Predeploys["GovernanceToken"] = &GovernanceTokenAddr
-	Predeploys["LegacyMessagePasser"] = &LegacyMessagePasserAddr
-	Predeploys["L2ERC721Bridge"] = &L2ERC721BridgeAddr
-	Predeploys["OptimismMintableERC721Factory"] = &OptimismMintableERC721FactoryAddr
-	Predeploys["ProxyAdmin"] = &ProxyAdminAddr
-	Predeploys["BaseFeeVault"] = &BaseFeeVaultAddr
-	Predeploys["L1FeeVault"] = &L1FeeVaultAddr
-	Predeploys["SchemaRegistry"] = &SchemaRegistryAddr
-	Predeploys["EAS"] = &EASAddr
-	Predeploys["ETH"] = &ETHAddr
-	Predeploys["Permit2"] = &Permit2Addr
-	Predeploys["QuoterV2"] = &QuoterV2Addr
-	Predeploys["SwapRouter02"] = &SwapRouter02Addr
-	Predeploys["UniswapV3Factory"] = &UniswapV3FactoryAddr
-	Predeploys["NFTDescriptor"] = &NFTDescriptorAddr
-	Predeploys["NonfungiblePositionManager"] = &NonfungiblePositionManagerAddr
-	Predeploys["NonfungibleTokenPositionDescriptor"] = &NonfungibleTokenPositionDescriptorAddr
-	Predeploys["TickLens"] = &TickLensAddr
-	Predeploys["UniswapInterfaceMulticall"] = &UniswapInterfaceMulticallAddr
-	Predeploys["L2UsdcBridge"] = &L2UsdcBridgeAddr
-	Predeploys["SignatureChecker"] = &SignatureCheckerAddr
-	Predeploys["MasterMinter"] = &MasterMinterAddr
-	Predeploys["FiatTokenV2_2"] = &FiatTokenV2_2Addr
+	Predeploys["LegacyERC20NativeToken"] = &Predeploy{
+		Address:       LegacyERC20NativeTokenAddr,
+		ProxyDisabled: true,
+	}
+	Predeploys["L2ToL1MessagePasser"] = &Predeploy{Address: L2CrossDomainMessengerAddr}
+	Predeploys["DeployerWhitelist"] = &Predeploy{Address: DeployerWhitelistAddr}
+	Predeploys["WNativeToken"] = &Predeploy{
+		Address:       WNativeTokenAddr,
+		ProxyDisabled: true,
+	}
+	Predeploys["L2CrossDomainMessenger"] = &Predeploy{Address: L2CrossDomainMessengerAddr}
+	Predeploys["L2StandardBridge"] = &Predeploy{Address: L2StandardBridgeAddr}
+	Predeploys["SequencerFeeVault"] = &Predeploy{Address: SequencerFeeVaultAddr}
+	Predeploys["OptimismMintableERC20Factory"] = &Predeploy{Address: OptimismMintableERC20FactoryAddr}
+	Predeploys["L1BlockNumber"] = &Predeploy{Address: L1BlockAddr}
+	Predeploys["GasPriceOracle"] = &Predeploy{Address: GasPriceOracleAddr}
+	Predeploys["L1Block"] = &Predeploy{Address: L1BlockAddr}
+	Predeploys["GovernanceToken"] = &Predeploy{
+		Address:       GovernanceTokenAddr,
+		ProxyDisabled: true,
+		Enabled: func(config DeployConfig) bool {
+			return config.GovernanceEnabled()
+		},
+	}
+	Predeploys["LegacyMessagePasser"] = &Predeploy{Address: LegacyMessagePasserAddr}
+	Predeploys["L2ERC721Bridge"] = &Predeploy{Address: L2ERC721BridgeAddr}
+	Predeploys["OptimismMintableERC721Factory"] = &Predeploy{Address: OptimismMintableERC721FactoryAddr}
+	Predeploys["ProxyAdmin"] = &Predeploy{Address: ProxyAdminAddr}
+	Predeploys["BaseFeeVault"] = &Predeploy{Address: BaseFeeVaultAddr}
+	Predeploys["L1FeeVault"] = &Predeploy{Address: L1FeeVaultAddr}
+	Predeploys["SchemaRegistry"] = &Predeploy{Address: SchemaRegistryAddr}
+	Predeploys["EAS"] = &Predeploy{Address: EASAddr}
+	Predeploys["ETH"] = &Predeploy{
+		Address:       ETHAddr,
+		ProxyDisabled: true,
+	}
+	Predeploys["QuoterV2"] = &Predeploy{Address: QuoterV2Addr}
+	Predeploys["SwapRouter02"] = &Predeploy{Address: SwapRouter02Addr}
+	Predeploys["UniswapV3Factory"] = &Predeploy{Address: UniswapV3FactoryAddr}
+	Predeploys["NFTDescriptor"] = &Predeploy{Address: NFTDescriptorAddr}
+	Predeploys["NonfungiblePositionManager"] = &Predeploy{Address: NonfungiblePositionManagerAddr}
+	Predeploys["NonfungibleTokenPositionDescriptor"] = &Predeploy{Address: NonfungibleTokenPositionDescriptorAddr}
+	Predeploys["TickLens"] = &Predeploy{Address: TickLensAddr}
+	Predeploys["UniswapInterfaceMulticall"] = &Predeploy{Address: UniswapInterfaceMulticallAddr}
+	Predeploys["L2UsdcBridge"] = &Predeploy{Address: L2UsdcBridgeAddr}
+	Predeploys["SignatureChecker"] = &Predeploy{
+		Address:       SignatureCheckerAddr,
+		ProxyDisabled: true,
+	}
+	Predeploys["MasterMinter"] = &Predeploy{
+		Address:       MasterMinterAddr,
+		ProxyDisabled: true,
+	}
+	Predeploys["FiatTokenV2_2"] = &Predeploy{Address: FiatTokenV2_2Addr}
+	Predeploys["Create2Deployer"] = &Predeploy{
+		Address:       Create2DeployerAddr,
+		ProxyDisabled: true,
+	}
+	Predeploys["MultiCall3"] = &Predeploy{
+		Address:       MultiCall3Addr,
+		ProxyDisabled: true,
+	}
+	Predeploys["Safe_v130"] = &Predeploy{
+		Address:       Safe_v130Addr,
+		ProxyDisabled: true,
+	}
+	Predeploys["SafeL2_v130"] = &Predeploy{
+		Address:       SafeL2_v130Addr,
+		ProxyDisabled: true,
+	}
+	Predeploys["MultiSendCallOnly_v130"] = &Predeploy{
+		Address:       MultiSendCallOnly_v130Addr,
+		ProxyDisabled: true,
+	}
+	Predeploys["SafeSingletonFactory"] = &Predeploy{
+		Address:       SafeSingletonFactoryAddr,
+		ProxyDisabled: true,
+	}
+	Predeploys["DeterministicDeploymentProxy"] = &Predeploy{
+		Address:       DeterministicDeploymentProxyAddr,
+		ProxyDisabled: true,
+	}
+	Predeploys["MultiSend_v130"] = &Predeploy{
+		Address:       MultiSend_v130Addr,
+		ProxyDisabled: true,
+	}
+	Predeploys["Permit2"] = &Predeploy{
+		Address:       Permit2Addr,
+		ProxyDisabled: true,
+	}
+	Predeploys["SenderCreator"] = &Predeploy{
+		Address:       SenderCreatorAddr,
+		ProxyDisabled: true,
+	}
+	Predeploys["EntryPoint"] = &Predeploy{
+		Address:       EntryPointAddr,
+		ProxyDisabled: true,
+	}
+
+	for _, predeploy := range Predeploys {
+		PredeploysByAddress[predeploy.Address] = predeploy
+	}
 }
