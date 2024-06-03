368d6758fe04f293911d56f5d14623b4396c404c
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-06-03 14:06:54 +0700

    OR-1629 update cannon

--- packages/tokamak/contracts-bedrock/src/cannon/MIPS.sol
+++ packages/tokamak/contracts-bedrock/src/cannon/MIPS.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity 0.8.15;
 
 import { IPreimageOracle } from "./interfaces/IPreimageOracle.sol";
 import { PreimageKeyLib } from "./PreimageKeyLib.sol";
