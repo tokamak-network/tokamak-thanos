b46f05efb344d4140c1f6dbe2cc30f290c794179
rlgns98kr <rlgns98kr@gmail.com>
2024-08-20 07:00:58 +0000

    update deprecated function

--- op-proposer/proposer/abi_test.go
+++ op-proposer/proposer/abi_test.go
@@ -10,10 +10,9 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/eth/ethconfig"
+	"github.com/ethereum/go-ethereum/ethclient/simulated"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/stretchr/testify/require"
 	"github.com/tokamak-network/tokamak-thanos/op-bindings/bindings"
@@ -30,12 +29,15 @@ func simulatedBackend() (privateKey *ecdsa.PrivateKey, address common.Address, o
 	if err != nil {
 		return nil, common.Address{}, nil, nil, err
 	}
-	backend = backends.NewSimulatedBackendFromConfig(ethconfig.Config{
-		Genesis: &core.Genesis{
-			Alloc:    types.GenesisAlloc{from: {Balance: big.NewInt(params.Ether)}},
-			GasLimit: 50_000_000,
-		},
-	})
+
+	b := simulated.NewBackend(types.GenesisAlloc{
+		from: {Balance: big.NewInt(params.Ether)},
+	}, simulated.WithBlockGasLimit(50_000_000))
+
+	backend = &backends.SimulatedBackend{
+		Backend: b,
+		Client:  b.Client(),
+	}
 
 	return privateKey, from, opts, backend, nil
 }
