8b4e841ce091b6bde8630e5a8c052189b2d0579a
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-08-31 00:24:20 +0700

    update data structure for onApprove

--- packages/tokamak/contracts-bedrock/src/L1/L1StandardBridge.sol
+++ packages/tokamak/contracts-bedrock/src/L1/L1StandardBridge.sol
@@ -153,22 +153,18 @@ contract L1StandardBridge is StandardBridge, OnApprove, ISemver {
     function unpackOnApproveData(bytes calldata _data)
         public
         pure
-        returns (address _from, address _to, uint256 _amount, uint32 _minGasLimit, bytes calldata _message)
+        returns (address _to, uint32 _minGasLimit, bytes calldata _message)
     {
-        require(_data.length >= 76, "Invalid onApprove data for L1StandardBridge");
+        require(_data.length >= 24, "Invalid onApprove data for L1StandardBridge");
         assembly {
             // The layout of a "bytes calldata" is:
-            // The first 20 bytes: _from
-            // The next 20 bytes: _to
-            // The next 32 bytes: _amount
+            // The first 20 bytes: _to
             // The next 4 bytes: _minGasLimit
             // The rest: _message
-            _from := shr(96, calldataload(_data.offset))
-            _to := shr(96, calldataload(add(_data.offset, 20)))
-            _amount := calldataload(add(_data.offset, 40))
-            _minGasLimit := shr(224, calldataload(add(_data.offset, 72)))
-            _message.offset := add(_data.offset, 76)
-            _message.length := sub(_data.length, 76)
+            _to := shr(96, calldataload(_data.offset))
+            _minGasLimit := shr(224, calldataload(add(_data.offset, 20)))
+            _message.offset := add(_data.offset, 24)
+            _message.length := sub(_data.length, 24)
         }
     }
 
@@ -188,10 +184,8 @@ contract L1StandardBridge is StandardBridge, OnApprove, ISemver {
     {
         address _nativeTokenAddress = nativeTokenAddress();
         require(msg.sender == _nativeTokenAddress, "only accept native token approve callback");
-        (address from, address to, uint256 amount, uint32 minGasLimit, bytes memory message) =
-            unpackOnApproveData(_data);
-        require(_owner == from && _amount == amount && amount > 0, "invalid onApprove data");
-        _initiateBridgeNativeToken(from, to, amount, minGasLimit, message);
+        (address to, uint32 minGasLimit, bytes memory message) = unpackOnApproveData(_data);
+        _initiateBridgeNativeToken(_owner, to, _amount, minGasLimit, message);
         return true;
     }
 
