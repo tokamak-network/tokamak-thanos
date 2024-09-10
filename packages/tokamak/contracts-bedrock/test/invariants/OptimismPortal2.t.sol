64b5eed70ba06d0c98f560e87af043de5ff55ef8
nguyenzung <nguyenvietdungcs52@gmail.com>
2024-09-11 00:49:34 +0700

    apply L2 native token for OptimismPortal2

--- packages/tokamak/contracts-bedrock/test/invariants/OptimismPortal2.t.sol
+++ packages/tokamak/contracts-bedrock/test/invariants/OptimismPortal2.t.sol
@@ -68,7 +68,7 @@ contract OptimismPortal2_Depositor is StdUtils, ResourceMetering {
             bound(_gasLimit, portal.minimumGasLimit(uint64(_data.length)), maxResourceLimit - cachedPrevBoughtGas)
         );
 
-        try portal.depositTransaction{ value: value }(_to, value, gasLimit, _isCreation, _data) {
+        try portal.depositTransaction(_to, value, value, gasLimit, _isCreation, _data) {
             // Do nothing; Call succeeded
         } catch {
             failedToComplete = true;
