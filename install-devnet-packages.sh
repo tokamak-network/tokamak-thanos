2a8772d94994d8c4874d0bebf0966979ef6cb184
ssaid <swywssaid@gmail.com>
2024-09-24 17:09:30 +0900

    [OR-1845] fix: Add docker apt update
    - update apt installed Docker packages

--- install-devnet-packages.sh
+++ install-devnet-packages.sh
@@ -575,17 +575,17 @@ elif [[ "$OS_TYPE" == "linux" ]]; then
             sudo apt-get install -y gnome-terminal
 
             # Add Docker's official GPG key:
-            sudo apt-get update -y
             sudo apt-get install -y ca-certificates curl
             sudo install -m 0755 -d /etc/apt/keyrings
             sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
             sudo chmod a+r /etc/apt/keyrings/docker.asc
-            # Add the repository to Apt sources:
 
+            # Add the repository to Apt sources:
             echo \
               "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
               $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
             # Install the Docker packages.
+            sudo apt-get update -y
             sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
             if [ "$SHELL_NAME" = "zsh" ]; then
