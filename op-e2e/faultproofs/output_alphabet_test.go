20c0e146ee83e78c8d779f9d5d1a6ff18c3f6e3c
Inphi <mlaw2501@gmail.com>
2024-11-01 08:55:50 -0700

    ci: Skip expensive fp e2e tests in main workflow (#12780)
    Co-authored-by: Matthew Slipper <me@matthewslipper.com>

--- op-e2e/faultproofs/output_alphabet_test.go
+++ op-e2e/faultproofs/output_alphabet_test.go
@@ -167,7 +167,7 @@ func TestOutputAlphabetGame_ValidOutputRoot(t *testing.T) {
 }
 
 func TestChallengerCompleteExhaustiveDisputeGame(t *testing.T) {
-	op_e2e.InitParallel(t)
+	op_e2e.InitParallel(t, op_e2e.IsSlow)
 
 	testCase := func(t *testing.T, isRootCorrect bool) {
 		ctx := context.Background()
