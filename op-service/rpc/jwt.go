d3a50acff5831aee28de941f3bd8bc9d06e6eff3
Axel Kingsley <axel.kingsley@gmail.com>
2024-12-18 09:49:57 -0600

    fix: First Time JWT Generation Bug (#13431)
    * fix first-time JWT generation bug
    
    * op-service: improve and test JWT secret loading code
    
    ---------
    
    Co-authored-by: protolambda <proto@protolambda.com>

--- op-service/rpc/jwt.go
+++ op-service/rpc/jwt.go
@@ -32,20 +32,29 @@ func ObtainJWTSecret(logger log.Logger, jwtSecretPath string, generateMissing bo
 				return eth.Bytes32{}, fmt.Errorf("JWT-secret in path %q does not exist: %w", jwtSecretPath, err)
 			}
 			logger.Warn("Failed to read JWT secret from file, generating a new one now.", "path", jwtSecretPath)
-			var secret eth.Bytes32
-			if _, err := io.ReadFull(rand.Reader, secret[:]); err != nil {
-				return eth.Bytes32{}, fmt.Errorf("failed to generate jwt secret: %w", err)
-			}
-			if err := os.WriteFile(jwtSecretPath, []byte(hexutil.Encode(secret[:])), 0o600); err != nil {
-				return eth.Bytes32{}, err
-			}
+			return generateJWTSecret(jwtSecretPath)
 		} else {
 			return eth.Bytes32{}, fmt.Errorf("failed to read JWT secret from file path %q", jwtSecretPath)
 		}
 	}
+	// Parse the JWT secret data we just read
 	jwtSecret := common.FromHex(strings.TrimSpace(string(data))) // FromHex handles optional '0x' prefix
 	if len(jwtSecret) != 32 {
 		return eth.Bytes32{}, fmt.Errorf("invalid jwt secret in path %q, not 32 hex-formatted bytes", jwtSecretPath)
 	}
 	return eth.Bytes32(jwtSecret), nil
+}
+
+// generateJWTSecret generates a new JWT secret and writes it to the file at the given path.
+// Prior status of the file is not checked, and the file is always overwritten.
+// Callers should ensure the file does not exist, or that overwriting is acceptable.
+func generateJWTSecret(path string) (eth.Bytes32, error) {
+	var secret eth.Bytes32
+	if _, err := io.ReadFull(rand.Reader, secret[:]); err != nil {
+		return eth.Bytes32{}, fmt.Errorf("failed to generate jwt secret: %w", err)
+	}
+	if err := os.WriteFile(path, []byte(hexutil.Encode(secret[:])), 0o600); err != nil {
+		return eth.Bytes32{}, err
+	}
+	return secret, nil
 }
