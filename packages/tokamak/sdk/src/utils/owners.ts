b9f1e3c96a86ba644613a04de8eb330708d1c0cb
Theo <theo@tokamak.network>
2025-02-19 17:36:19 +0900

    add LocalDevnet owners for testing

--- packages/tokamak/sdk/src/utils/owners.ts
+++ packages/tokamak/sdk/src/utils/owners.ts
@@ -1,6 +1,7 @@
 export enum Chains {
   Mainnet = 1,
   Sepolia = 11155111,
+  LocalDevnet = 900
 }
 
 /**
@@ -24,6 +25,12 @@ export const getDAOMembers = (chainid: number): [string, string] => {
       '0x0Fd5632f3b52458C31A2C3eE1F4b447001872Be9',
       '0x61dc95E5f27266b94805ED23D95B4C9553A3D049',
       // '0xA2101482b28E3D99ff6ced517bA41EFf4971a386', // TokamakDAO
+    ]
+  } else if (chainid === Chains.LocalDevnet) {
+    // LocalDevnet (for testing)
+    return [
+      '0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65', // devAccount-4
+      '0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc' // devAccount-5
     ]
   } else {
     throw new Error('It is not a supported chain')
