57f78e1bd11862ad46efbf4e1414765f20c53e87
Adrian Sutton <adrian@oplabs.co>
2025-03-11 00:52:42 +1000

    op-program: Update prestate reproducibility check to compare against list of prestates from superchain-registry (#14744)
    * op-program: Switch prestate verification to use the superchain-registry as source of truth
    
    * op-program: Update prestate reproducibility check to reference the same list superchain-registry now uses
    
    * Fix script
    
    * ci: Reduce timeout for prestate builds again.
    
    * Clarify cannon 32 was built.
    
    * Remove TODO

--- op-program/scripts/build-prestates.sh
+++ op-program/scripts/build-prestates.sh
@@ -48,8 +48,8 @@ do
     else
       cp "${BIN_DIR}/prestate.json" "${STATES_DIR}/${HASH}.json"
     fi
-    VERSIONS_JSON=$(echo "${VERSIONS_JSON}" | jq ". += [{\"version\": \"${SHORT_VERSION}\", \"hash\": \"${HASH}\"}]")
-    echo "Built ${VERSION}: ${HASH}"
+    VERSIONS_JSON=$(echo "${VERSIONS_JSON}" | jq ". += [{\"version\": \"${SHORT_VERSION}\", \"hash\": \"${HASH}\", \"type\": \"cannon32\"}]")
+    echo "Built cannon32 ${VERSION}: ${HASH}"
 
     if [ -f "${BIN_DIR}/prestate-proof-mt64.json" ]; then
       HASH=$(cat "${BIN_DIR}/prestate-proof-mt64.json" | jq -r .pre)
