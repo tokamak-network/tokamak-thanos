06a42f54515c4b46c4e8a8e25999b41778e61583
Theo <theo@tokamak.network>
2025-08-20 18:42:57 +0900

    Update DAO member addresses for Sepolia

--- packages/tokamak/sdk/src/utils/owners.ts
+++ packages/tokamak/sdk/src/utils/owners.ts
@@ -15,16 +15,14 @@ export const getDAOMembers = (chainid: number): [string, string] => {
   if (chainid === Chains.Mainnet) {
     // Mainnet
     return [
-      '0x0Fd5632f3b52458C31A2C3eE1F4b447001872Be9',
-      '0x61dc95E5f27266b94805ED23D95B4C9553A3D049',
-      // '0xDD9f0cCc044B0781289Ee318e5971b0139602C26', // TokamakDAO
+      '0x0Fd5632f3b52458C31A2C3eE1F4b447001872Be9', // dummy
+      '0xDD9f0cCc044B0781289Ee318e5971b0139602C26', // TokamakDAO
     ]
   } else if (chainid === Chains.Sepolia) {
     // Sepolia
     return [
-      '0x0Fd5632f3b52458C31A2C3eE1F4b447001872Be9',
-      '0x61dc95E5f27266b94805ED23D95B4C9553A3D049',
-      // '0xA2101482b28E3D99ff6ced517bA41EFf4971a386', // TokamakDAO
+      '0xeefb04f3a4406363b399a1c3d194d75ca81a2d1b', // Foundation
+      '0xA2101482b28E3D99ff6ced517bA41EFf4971a386', // TokamakDAO
     ]
   } else if (chainid === Chains.LocalDevnet) {
     // LocalDevnet (for testing)
